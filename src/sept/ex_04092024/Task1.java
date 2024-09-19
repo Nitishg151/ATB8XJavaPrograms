d55850d23caa6f5b7a2b4f8df57342292e9c46b5
Nitish <nitishg151@gmail.com>
2024-09-19 16:25:28 +0530

    Add this files

---
+++ src/sept/ex_04092024/Task1.java
@@ -0,0 +1,18 @@
+package sept.ex_04092024;
+
+public class Task1 {
+
+
+
+    public static void main(String args[]) {
+    double a=34;
+    double b=10;
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+
+    }
+}
+
